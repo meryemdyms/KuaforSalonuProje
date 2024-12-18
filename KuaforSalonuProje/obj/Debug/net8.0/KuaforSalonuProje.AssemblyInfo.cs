4164a5ecea346a8f57aff71e19cf72a7401a9505
aysegullkara <aysegulkaraa3@gmail.com>
2024-12-18 23:36:08 +0300

    Frontend tarafında iyileştirmeler yapıldı

--- KuaforSalonuProje/obj/Debug/net8.0/KuaforSalonuProje.AssemblyInfo.cs
+++ KuaforSalonuProje/obj/Debug/net8.0/KuaforSalonuProje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1420ad347a633b7c4b6077677315f135298bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
