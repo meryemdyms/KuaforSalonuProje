8d607e6e3aaed39adc51959db0cf914793ac81c8
meryem <meryem.duymus2@ogr.sakarya.edu.tr>
2024-12-18 13:54:05 +0300

    Proje dosyaları eklendi

---
+++ KuaforSalonuProje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuaforSalonuProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
