d68f842800bf8f3a002b3a55ce4a52fe57a855ee
meryem <meryem.duymus2@ogr.sakarya.edu.tr>
2024-12-20 17:18:12 +0300

    Login işlemleri için giriş ekranı tasarımı oluşturuldu

--- KuaforSalonuProje/Controllers/AccountController.cs
+++ KuaforSalonuProje/Controllers/AccountController.cs
@@ -1,90 +1,12 @@
-﻿using KuaforSalonuProje.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace KuaforSalonuProje.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly UserManager<Kullanici> _userManager;
-        private readonly SignInManager<Kullanici> _signInManager;
-
-        public AccountController(UserManager<Kullanici> userManager, SignInManager<Kullanici> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(Kullanici model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new Kullanici
-                {
-                    Adi = model.Email,
-                    Email = model.Email,
-                    Rol = Roller.Kullanici // Default olarak 'Kullanici' rolü
-                };
-                var result = await _userManager.CreateAsync(user, model.Sifre);
-
-                if (result.Succeeded)
-                {
-                    // Admin kontrolü eklemek
-                    if (user.Rol == Roller.Admin)
-                    {
-                        await _userManager.AddToRoleAsync(user, Roller.Admin);
-                    }
-                    else
-                    {
-                        await _userManager.AddToRoleAsync(user, Roller.Kullanici);
-                    }
-
-                    return RedirectToAction("Login");
-                }
-                else
-                {
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError("", error.Description);
-                    }
-                }
-            }
-            return View(model);
-        }
-
-        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-                if (user != null)
-                {
-                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
-
-                    if (result.Succeeded)
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                }
-            }
-            return View(model);
-        }
-
-
-
     }
 }
-
