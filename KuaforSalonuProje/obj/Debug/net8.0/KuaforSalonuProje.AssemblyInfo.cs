fd9e29634eacc2338baa9ffc8cce1b66682c6a34
meryem <meryem.duymus2@ogr.sakarya.edu.tr>
2024-12-28 14:27:10 +0300

    Güncelleme yapıldı

--- KuaforSalonuProje/obj/Debug/net8.0/KuaforSalonuProje.AssemblyInfo.cs
+++ KuaforSalonuProje/obj/Debug/net8.0/KuaforSalonuProje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b8f4e9ba75257aafb3915a390138854afbd193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46747a93f019e87663340519f86db049fc24a41f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KuaforSalonuProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
